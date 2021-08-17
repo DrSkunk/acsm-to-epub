4f6a37cf11f36fa3c6c9137a385038875c54ae5e
Sebastiaan Jansen <contact@sebastiaanjansen.be>
2021-08-17 22:03:08 +0200

    port

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 
+const port = process.env.PORT || 3945;
+
 var corsOptions = {
   origin: "http://example.com",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
@@ -15,6 +17,6 @@ app.get("/products/:id", cors(corsOptions), function (req, res, next) {
   res.json({ msg: "This is CORS-enabled for only example.com." });
 });
 
-app.listen(3945, function () {
-  console.log("CORS-enabled web server listening");
+app.listen(port, function () {
+  console.log(`CORS-enabled web server listening on ${port}`);
 });
